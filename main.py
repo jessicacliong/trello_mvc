1c8094c09f9d1a2a769984f8673befa449553501
jessicacliong <14395@coderacademy.edu.au>
2023-06-17 10:59:06 +1000

    Create cli command for creating, dropping tables and seeding users

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask
 import os
 from init import db, ma, bcrypt, jwt
+from controllers.cli_controllers import db_commands
 
 def create_app():
   app = Flask(__name__)
 
-  # Create                                dbms      driver      user        pw    url      port   db name
+  
   app.config["SQLALCHEMY_DATABASE_URI"]=os.environ.get("DATABASE_URL")
   app.config["JWT_SECRET_KEY"]=os.environ.get("JWT_SECRET_KEY")
 
@@ -13,5 +14,7 @@ def create_app():
   ma.init_app(app)
   bcrypt.init_app(app)
   jwt.init_app(app)
+
+  app.register_blueprint(db_commands)
 
   return app
