e01924d35b0483a40b1f42f4d5a53bad1434f3dc
jessicacliong <14395@coderacademy.edu.au>
2023-06-23 07:57:46 +1000

    Add Card Model, and relate user and card

--- models/user.py
+++ models/user.py
@@ -1,4 +1,5 @@
 from init import db, ma
+from marshmallow import fields
 
 class User(db.Model):
   __tablename__ = 'users'
@@ -9,13 +10,14 @@ class User(db.Model):
   password = db.Column(db.String, nullable=False)
   is_admin = db.Column(db.Boolean, default=False)
 
+  cards = db.relationship('Card', back_populates='user', cascade='all, delete')
+
 class UserSchema(ma.Schema):
   cards = fields.List(fields.Nested('CardSchema', exclude=['user']))
-  #exclude = the schema doesn't include that field
+  # exclude = the schema doesn't include that field
   #here its done to prevent recursion, user calls cards which calls user which calls cards ...
-
   class Meta:
-    fields = ('id', 'name', 'email', 'password', 'is_admin')
+    fields = ('id', 'name', 'email', 'password', 'is_admin', 'cards')
 
 user_schema = UserSchema(exclude=['password'])
 users_schema = UserSchema(many=True, exclude=['password'])
