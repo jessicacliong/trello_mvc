c2a9e89c870681b4a5562750984c140d4f485811
jessicacliong <14395@coderacademy.edu.au>
2023-06-29 20:56:12 +1000

    Comment Controllers

--- models/card.py
+++ models/card.py
@@ -12,7 +12,8 @@ class Card(db.Model):
   priority = db.Column(db.String)
 
   user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-
+  
+  #connects cards model to the user model in user.py
   user = db.relationship('User', back_populates='cards')
   comments = db.relationship('Comment', back_populates='card', cascade='all, delete')
 
